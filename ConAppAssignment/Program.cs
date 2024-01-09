c4938a6b2272dc746c077b57eef3090171ba8558
Naveenn789 <naveengalla77@gmail.com>
2024-01-09 14:52:00 +0530

    'Jenkins'

---
+++ ConAppAssignment/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("First i Downloaded and installed jenkins in my machine");
+Console.WriteLine("And started working on it");
+Console.WriteLine("This is my Jenkins Assignment Project");
